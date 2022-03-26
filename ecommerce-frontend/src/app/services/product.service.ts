b4ba7708afbc95924584e29867ec5b102c3feff0
Rezvantsev-A <Rezvantsev-A@gaz-is.ru>
2022-03-26 14:16:47 +0300

    product list and service

---
+++ ecommerce-frontend/src/app/services/product.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Product} from "../common/product";
+import {map, Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'http://localhost:8080/api'
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient
+      .get<ProductsResponse>(this.baseUrl + '/products')
+      .pipe(
+        map(response => response._embedded.products)
+      );
+  }
+}
+
+interface ProductsResponse {
+  _embedded: {
+    products: Product[]
+  }
+}
